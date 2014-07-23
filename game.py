7e078e6183562506e88f7c031a305a37daad0e28
leftynight <ryan.a.lund@gmail.com>
2014-07-23 14:26:42 -0400

    Part deux

---
+++ game.py
@@ -0,0 +1,132 @@
+# Pygame imports
+import pygame, pygame.mixer
+from pygame import Surface
+from pygame.image import load
+from pygame.locals import *
+from pygame.mixer import music
+from pygame.rect import Rect
+from pygame.sprite import Group, Sprite
+
+# Path imports
+from os.path import join
+
+# Random imports
+from random import randint, choice
+
+# Microgame-specific imports
+import locals
+from microgame import Microgame
+
+##### LOADER-REQUIRED FUNCTIONS ################################################
+
+def make_game():
+    # TODO: Return a new instance of your Microgame class.
+    #raise NotImplementedError("make_game")
+    return LumberJack()
+
+def title():
+    # TODO: Return the title of the game.
+    #raise NotImplementedError("title")
+    return "Lumber Jack"
+
+def thumbnail():
+    # TODO: Return a (relative path) to the thumbnail image file for your game.
+    #raise NotImplementedError("thumbnail")
+    return join('games','Lumber_Jack','thumbnail.png')
+
+def hint():
+    # TODO: Return the hint string for your game.
+    #raise NotImplementedError("hint")
+    return "Chop the tree as fast as you can"
+
+################################################################################
+
+def _load_image(name, x, y):
+    '''
+    Loads an image file, returning the surface and rectangle corresponding to
+    that image at the given location.
+    '''
+    try:
+        image = load(name)
+        if image.get_alpha is None:
+            image = image.convert()
+        else:
+            image = image.convert_alpha()
+    except pygame.error, msg:
+        print 'Cannot load image: {}'.format(name)
+        raise SystemExit, msg
+    rect = image.get_rect().move(x, y)
+    return image, rect
+
+##### MODEL CLASSES ############################################################
+
+# TODO: put your Sprite classes here
+class Man(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        postion = 0 #0 = left, 1 = right
+        imgpath = join('games', 'Lumber_Jack', 'jack.gif')
+        self.image, self.rect = _load_image(imgpath, 300, 300)#locals.HEIGHT)
+        #self.sound = pygame.mixer.Sound(join( ... ))
+
+class treeBlock(Sprite):
+    def __init__(self, x, y, type):
+        Sprite.__init__(self)
+        if  self.type == 0:
+            imgpath = join('games', 'Lumber_Jack', 'tree_right.png')
+
+        elif self.type == 1:
+            imgpath = join('games', 'Lumber_Jack', 'tree_left.png')
+            x -= 75
+        else:
+            imgpath = join('games', 'Lumber_Jack', 'tree_center.png')
+
+        self.image, self.rect = _load_image(imgpath, x, y)
+
+    #def update(self, event):
+        #self.sound.play()
+        
+class tree(Sprite):
+    def __init(self):
+        Sprite.__init__(self)
+        createTree(100, locals.HEIGHT)
+
+    def createTree(x,y):
+        self.Tree = []
+        x, y = tree.rect.topleft
+        while y > 0:
+            self.agrigate.add(treeBlock(x-75, y), randint(0,4))
+
+
+##### MICROGAME CLASS ##########################################################
+
+# TODO: rename this class to your game's name...
+class LumberJack(Microgame):
+    def __init__(self):
+        Microgame.__init__(self)
+        self.jack = Man()
+        self.tree = tree()
+        self.sprites = Group(self.jack, self.tree)
+        # TODO: Initialization code here
+
+    def start(self):
+        # TODO: Startup code here
+        pass
+
+    def stop(self):
+        # TODO: Clean-up code here
+        pass
+
+    def update(self, events):
+        # TODO: Update code here
+        self.sprites.update()
+
+    def render(self, surface):
+        # TODO: Rendering code here
+        surface.fill(Color(255, 255, 255))
+        self.sprites.draw(surface)
+
+    def get_timelimit(self):
+        # TODO: Return the time limit of this game (in seconds, 0 <= s <= 15)
+        #raise NotImplementedError("get_timelimit")
+        return 15
