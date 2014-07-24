25cdf75f3f5a70e4af71c253522183f3b8f4fdc9
drerad <drerad22@gmai.com>
2014-07-24 14:28:12 -0400

    Update game.py

--- game.py
+++ game.py
@@ -32,7 +32,7 @@ def title():
 def thumbnail():
     # TODO: Return a (relative path) to the thumbnail image file for your game.
     #raise NotImplementedError("thumbnail")
-    return join('games','Lumberjack',"images",'thumbnail.jpg')
+    return join('games','Lumberjack',"images",'thumbnail.png')
 
 def hint():
     # TODO: Return the hint string for your game.
@@ -66,7 +66,7 @@ class Man(Sprite):
         Sprite.__init__(self)
         postion = 0 #0 = left, 1 = right
         imgpath = join('games', 'Lumberjack',"images", "timberman_normalleft.png")
-        self.image, self.rect = _load_image(imgpath, 150, 400)#locals.HEIGHT)
+        self.image, self.rect = _load_image(imgpath, 105, 400)#locals.HEIGHT)
         #self.sound = pygame.mixer.Sound(join( ... ))
 
 class treeBlock(Sprite):
@@ -99,14 +99,15 @@ class LumberjackGame(Microgame):
         # TODO: Initialization code here
         Microgame.__init__(self)
         self.jack = Man()
-        self.left, _ = _load_image(join("games","Lumberjack","images","timberman_normalleft.png"), 150, 400)
-        self.left_chop, _ = _load_image(join("games","Lumberjack","images","timberman_chopleft.png"), 150, 400)
-        self.right, _ = _load_image(join("games","Lumberjack","images","timberman_normalright.png"), 150, 400)
-        self.right_chop, _ = _load_image(join("games","Lumberjack","images","timberman_chopright.png"), 150, 400)
+        self.count = 0
+        self.left, self.left_rect = _load_image(join("games","Lumberjack","images","timberman_normalleft.png"), 105, 400)
+        self.left_chop, self.leftc_rect = _load_image(join("games","Lumberjack","images","timberman_chopleft.png"), 130, 450)
+        self.right, self.right_rect = _load_image(join("games","Lumberjack","images","timberman_normalright.png"), 500, 400)
+        self.right_chop, self.rightc_rect = _load_image(join("games","Lumberjack","images","timberman_chopright.png"), 375, 450)
         self.sprites = Group(self.jack)
-        self.background = load(join("games","Lumberjack","images","forest.jpg"))
+        self.background = load(join("games","Lumberjack","images","forest.png"))
         self.tree = Group(treeBlock(LEFT_POSITION, 550, 1))
-        
+  
     def start(self):
         # TODO: Startup code here
         #music.load(join("games","Lumberjack","music","tree_song.ogg"))
@@ -162,20 +163,22 @@ class LumberjackGame(Microgame):
         sound_chop = pygame.mixer.Sound(join("games","Lumberjack","music","axe_chop.wav"))
         if self.jack.image == self.left_chop:
             self.jack.image = self.left
-            self.jack.rect = (105, 400)
+            self.jack.rect = self.left_rect
         elif self.jack.image == self.right_chop:
             self.jack.image = self.right
-            self.jack.rect = (500, 400)
+            self.jack.rect = self.right_rect
         for event in events:
             if event.type == KEYDOWN and event.key == K_LEFT:
+                self.count += 1
                 sound_chop.play()
                 self.jack.image = self.left_chop
-                self.jack.rect = (130, 450)
+                self.jack.rect = self.leftc_rect
                 self.updateTree(2)
             elif event.type == KEYDOWN and event.key == K_RIGHT:
+                self.count += 1
                 sound_chop.play()
                 self.jack.image = self.right_chop
-                self.jack.rect = (375, 450)
+                self.jack.rect = self.rightc_rect
                 self.updateTree(0)
 
     def render(self, surface):
